10ba49ae9e5d4424bb9c23a357a31285f7f78ab7
Subahu Rayamajhi <subahur@brandeis.edu>
2015-10-18 19:05:51 -0400

    formatted new task page

--- app/src/main/java/com/example/snr/errand_helper/MainActivity.java
+++ app/src/main/java/com/example/snr/errand_helper/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     public static Button button_new_task_submit;
-
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
