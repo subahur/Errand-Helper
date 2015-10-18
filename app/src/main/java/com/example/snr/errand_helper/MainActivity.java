54e1771158137e295258e214adca69e9cf4c5bb2
Subahu Rayamajhi <subahur@brandeis.edu>
2015-10-17 23:46:03 -0400

    try

--- app/src/main/java/com/example/snr/errand_helper/MainActivity.java
+++ app/src/main/java/com/example/snr/errand_helper/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //try
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
