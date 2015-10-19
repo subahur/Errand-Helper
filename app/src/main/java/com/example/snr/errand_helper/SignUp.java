7cc1e04c8d06ea43ff7c209ed89b4b7461a7202f
k3rz0rg <saad.andalib.88@live.com>
2015-10-19 19:42:21 -0400

    < created the new page called signup.xml
    takes the username and email is prefixed, password and retype password and ok box
    
    />

---
+++ app/src/main/java/com/example/snr/errand_helper/SignUp.java
@@ -0,0 +1,17 @@
+package com.example.snr.errand_helper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by Saad Andalib on 10/19/2015.
+ */
+
+public class SignUp extends AppCompatActivity {
+
+    public static Button button_sign_up;
+
+
+
+
+}
