5338a629d67b93c88ae95f49a893af11caa34143
Subahu Rayamajhi <subahur@brandeis.edu>
2015-10-19 20:02:02 -0400

    login to home page with welcome username meassage

--- app/src/main/java/com/example/snr/errand_helper/MainActivity.java
+++ app/src/main/java/com/example/snr/errand_helper/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    //
+    ///
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
