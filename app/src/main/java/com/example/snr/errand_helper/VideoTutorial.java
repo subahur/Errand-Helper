4f8beb3a026788dc3b131780e9616dae49e80712
Subahu Rayamajhi <subahur@brandeis.edu>
2015-12-04 16:06:01 -0500

    vid new link

--- app/src/main/java/com/example/snr/errand_helper/VideoTutorial.java
+++ app/src/main/java/com/example/snr/errand_helper/VideoTutorial.java
@@ -17,13 +17,14 @@ public class VideoTutorial extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_tutorial);
         VideoView vidView = (VideoView)findViewById(R.id.myVideo);
-        Uri vidUri = Uri.parse(TUTORIAL_URL);
-        //vidView.setVideoPath("/sdcard/skeleton.mp4");
-        vidView.setVideoURI(vidUri);
+        //Uri vidUri = Uri.parse(TUTORIAL_URL);
+        vidView.setVideoPath("/sdcard/skeleton.mp4");
+       // vidView.setVideoURI(vidUri);
         MediaController vidControl = new MediaController(this);
         vidControl.setAnchorView(vidView);
         vidView.setMediaController(vidControl);
         vidView.start();
+        vidView.requestFocus();
 
     }
 
